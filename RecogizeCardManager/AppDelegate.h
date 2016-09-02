05c5119ed290b6253e26e970d0b46bee03b96961
AndyLi2020 <Xiaomei@encompass8.cn>
2016-09-02 20:43:25 +0800

    Initial Commit

---
+++ RecogizeCardManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RecogizeCardManager
+//
+//  Created by Xiaomei LI on 9/2/16.
+//  Copyright © 2016 Xiaomei LI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RecogizeCardManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RecogizeCardManager
+//
+//  Created by Xiaomei LI on 9/2/16.
+//  Copyright © 2016 Xiaomei LI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
